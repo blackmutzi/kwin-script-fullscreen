e00d2c3deba3d22db43431fff0cb60f75776022b
blackmutzi <this.mutzii@gmail.com>
2016-09-27 12:35:29 +0000

    Support Flash Player (plugin-container)

--- contents/code/fullscreen.js
+++ contents/code/fullscreen.js
@@ -1,20 +1,35 @@
 var x = 0;
 var y = 0;
-
+var screen_resolution_width  = 1920;
+var screen_resolution_height = 1080;
 
 workspace.clientActivated.connect(function(client) {
   
-  if (client === null) {
-    return;
-  } 
+  if (client === null) 
+  {
+      return;      
+  }
+	
+  if( client.caption != "plugin-container" )
+  {
+	  
+      x = eval( client.geometry.x );
+      y = eval( client.geometry.y );
   
-  x = eval( client.geometry.x );
-  y = eval( client.geometry.y );
-  
+     
+  }else
+  {
+	 /* print("is a plugin-container");*/
+	  
+	 FullScreenEvent( client , true , false );
+  }
   
 });
+
 var FullScreenEvent = function (client, fullscreen, user) {
 	
+	/* print("fullscreen Event started"); */
+ 	
 	var geometry    = client.geometry;
 	var clientPos   = client.clientPos;
     var full        = client.fullScreen;
@@ -22,22 +37,28 @@ var FullScreenEvent = function (client, fullscreen, user) {
 	
     if (client.fullScreen) 
     {
-		geometry.width  = 1920;
-	    geometry.height = 1080;
+		geometry.width  = screen_resolution_width;
+	    geometry.height = screen_resolution_height;
 				
-		if( x < 1920 )
+		if( x < screen_resolution_width )     /* Monitor 1 */
 		{
 			geometry.x    = 0;
 		    geometry.y    = 0;
 		    
-		}else{
+		}else if( x > screen_resolution_width && x < screen_resolution_width * 2 )  /* Monitor 2 */
+		{
 		
-		    geometry.x   = 1920;
+		    geometry.x   = screen_resolution_width ;
 		    geometry.y   = 0;	
 				
+		}else{								  /* Monitor 3 */
+			
+			geometry.x  = screen_resolution_width * 2;
+			geometry.y  = 0;
 		}
-		
     }	
-	   client.geometry  = geometry;  
+	
+	/* update geometry */   
+    client.geometry  = geometry;  
 };
 workspace.clientFullScreenSet.connect(FullScreenEvent);
